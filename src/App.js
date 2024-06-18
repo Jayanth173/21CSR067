2e3e83cd09ebaaf3d82f6470a7d1be78ec0d53b2
JAYANTH J R <93864425+Jayanth173@users.noreply.github.com>
2024-06-18 12:34:30 +0530

    first

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>E-commerce Product List</h1>
+        <h1><center>E-commerce Product List</center></h1>
       </header>
       <ProductList />
     </div>
